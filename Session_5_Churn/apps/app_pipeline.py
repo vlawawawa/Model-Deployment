266aa49395b8b32d52d328c72edcbe48d928f382
valen <turtlepen1@gmail.com>
2026-03-10 09:14:42 +0700

    Update app_pipeline.py

--- Session_5_Churn/apps/app_pipeline.py
+++ Session_5_Churn/apps/app_pipeline.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import streamlit as st
 
+##########
+
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+##########
+
 from config.config import ARTIFACT_PIPELINE
 from src.utils.io import load_artifact
 
